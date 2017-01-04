fa1185b340186e1ee7145c886f9110bc4ff1b0bc
vogtn <vogtn@uw.edu>
2017-01-03 20:47:39 -0800

    adding user functionality

--- app.js
+++ app.js
@@ -11,5 +11,19 @@ app.controller('MainCtrl', [
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
+
+    $scope.addPost = function() {
+      if(!$scope.title || $scope.title === ''){return;}
+      $scope.posts.push({
+        title: $scope.title,
+        link:  $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
   }
 ]);
