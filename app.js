ceabf2ce53fe7a3678fa82ffa3b37bf8878a8ac0
vogtn <vogtn@uw.edu>
2017-01-04 12:39:54 -0800

    adding express scaffolding

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.config([
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
       });
       $urlRouterProvider.otherwise('home')
   }
@@ -20,7 +25,24 @@ app.factory('posts', [function(){
     posts: []
   };
   return o;
-}])
+}]);
+
+app.controller('PostsCtrl',[
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if($scope.body === ''){return;}
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    }
+}]);
 app.controller('MainCtrl', [
   '$scope',
   'posts',
@@ -33,7 +55,11 @@ app.controller('MainCtrl', [
       $scope.posts.push({
         title: $scope.title,
         link:  $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
